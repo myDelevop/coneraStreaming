ebe7a35a053f5cdd87a065693acfc5ad37401463
rock_ <rock_@DESKTOP-HLS4NEU.homenet.telecomitalia.it>
2016-10-21 16:41:15 +0200

    insert AverageAttribute average collective schema

---
+++ src/data/schema/AverageAttribute.java
@@ -0,0 +1,98 @@
+package data.schema;
+
+import java.util.Iterator;
+
+import data.dataInstance.Edge;
+import data.dataInstance.Neighbourhood;
+import data.dataInstance.Node;
+
+public class AverageAttribute extends CollectiveAttribute {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    
+    private double nSize = 0; // Σ(w)
+    private double weightedNsize = 0; // Σ(w*yj)  
+    
+    
+    private WeightI wI;
+
+    public AverageAttribute(String name, int index, WeightI wI) {
+        super(name, index);
+        this.wI = wI;
+    }
+
+    @Override
+    public double get() {
+        if(weightedNsize==0) return 0;
+
+        return weightedNsize/nSize;
+    }
+
+    private void update(Neighbourhood nh) {
+        
+        for(Edge e:nh) {
+            
+            Node to = e.getTo();
+            //  System.out.println("Y"+to.getTarget()+ " PY"+to.getPredictedTarget());
+            double w = wI.getWeight(e.getDistance());
+            double y = 0;
+            if (to.isSample() || to.isWorkingToSample()) {
+                y = (double) to.getTarget().getValue();
+                nSize = nSize + w;
+                weightedNsize = weightedNsize + (w*y); 
+            } // IF WE WOULD ALSO CONSIDER UNLABELED NODES: 
+            /*else { 
+                y = (double) to.getPredictedTarget().getValue();
+                nSize = nSize + w;
+                weightedNsize = weightedNsize + (w*y); 
+            }*/
+
+        }
+        
+    }
+    
+    public void compute(Neighbourhood nh) {
+        nSize = 0;
+        weightedNsize = 0;
+        
+        update(nh);
+    }
+    
+    
+    public void compute(Neighbourhood nh, AverageAttribute a) {
+        nSize = a.nSize;
+        weightedNsize = a.weightedNsize;
+        update(nh);
+        Iterator<Edge> it = nh.externalIterator();
+        while(it.hasNext()) {
+            Edge e = it.next();
+            Node to = e.getTo();
+            //  System.out.println("Y"+to.getTarget()+ " PY"+to.getPredictedTarget());
+            double w = wI.getWeight(e.getDistance());
+            double y = 0;
+            if (to.isSample() || to.isWorkingToSample()) {
+                y = (double) to.getTarget().getValue();
+                nSize = nSize + w;
+                weightedNsize = weightedNsize + (w*y); 
+            } // IF WE WOULD ALSO CONSIDER UNLABELED NODES: 
+            /*else { 
+                y = (double) to.getPredictedTarget().getValue();
+                nSize = nSize + w;
+                weightedNsize = weightedNsize + (w*y); 
+            }*/   
+        }
+    }
+    
+    public String getName(){
+        return super.getName();
+    }
+
+    
+    public String toString() {
+        return getName();
+    }
+}
